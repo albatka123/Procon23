4ef90ca74e298e25247ac2a314ed9cacf6c31f15
Le Minh Bao <leminhbaost@gmail.com>
2024-04-15 01:03:57 +0700

    Khoi tao ban co

---
+++ Procon23/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Procon23.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Procon23.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Tạo một đối tượng ChessboardModel
+            var model = new ChessboardModel(rows: 8, columns: 8); // Ví dụ: Bàn cờ 8x8
+
+            // Trả về view "Index" và truyền model tới view
+            return View(model);
+        } 
+    }
+}
---
+++ Procon23/Models/ChessboardModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Procon23.Models
+{
+    public class ChessboardModel
+    {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public string[,] Squares { get; set; }
+
+        // Các loại khu vực
+        public enum AreaType { Neutral, Territory, Castle, Wall, Pond }
+        public AreaType[,] Areas { get; set; }
+
+        public ChessboardModel(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            Squares = new string[Rows, Columns];
+            Areas = new AreaType[Rows, Columns];
+           
+
+            // Khởi tạo giá trị ban đầu cho các loại khu vực (Trung lập)
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Areas[i, j] = AreaType.Neutral;
+                }
+            }
+
+        }
+    }
+}
